0a4a522c53b543286dbdca3f631d71865fa385ea
Ahmad Saifuddin <e.din0210@gmail.com>
2013-11-12 15:59:46 +0800

    Initial Commit

---
+++ time-tester/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  time-tester
+//
+//  Created by saifuddin on 12/11/13.
+//  Copyright (c) 2013 saifuddin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
